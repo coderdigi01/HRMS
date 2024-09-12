249aa602ea6578612477a0d59d0449d416b5bb58
Tarun kumar <tarun.kumar@tech58.co.in>
2024-09-12 11:10:21 +0530

    folder change

--- hr/views.py
+++ hr/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 
 def home(request):
     # return render(request, 'hr/home.html.j2')
-    return render(request, 'index.html.j2')
+    return render(request, 'hr/index.html.j2')
 
 
 def dashboard(request):
-    return render(request, 'dashboard.html.j2')
+    return render(request, 'hr/dashboard.html.j2')
